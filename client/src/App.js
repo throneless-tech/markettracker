4a243a0329a2f729264debc3ec0502c6276a08e9
rudietuesdays <rae@throneless.tech>
2023-04-03 16:34:21 -0400

    begin build in chakra ui

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { Logo } from './Logo';
+import { Dashboard } from './components/dashboard';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <Dashboard />
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
