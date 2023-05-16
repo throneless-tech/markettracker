d783a55877e96b9cba391c06ddb3cd90dd3a397e
Harum Helmy <harumendhah@gmail.com>
2023-05-16 14:01:05 -0400

    Update file path on import statements in App.js to fix errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,35 @@
 import React from 'react';
-import '@fontsource/outfit/100.css'
-import '@fontsource/outfit/400.css'
-import '@fontsource/outfit/500.css'
-import '@fontsource/outfit/600.css'
-import '@fontsource/outfit/700.css'
-import '@fontsource/outfit/800.css'
-import '@fontsource/zilla-slab/400.css'
-import '@fontsource/zilla-slab/700.css'
+import '@fontsource/outfit/100.css';
+import '@fontsource/outfit/400.css';
+import '@fontsource/outfit/500.css';
+import '@fontsource/outfit/600.css';
+import '@fontsource/outfit/700.css';
+import '@fontsource/outfit/800.css';
+import '@fontsource/zilla-slab/400.css';
+import '@fontsource/zilla-slab/700.css';
 import {
   ChakraProvider,
   Box,
   Tabs,
   TabPanel,
-  TabPanels
+  TabPanels,
 } from '@chakra-ui/react';
 import theme from './theme';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Dashboard } from './Components/Dashboard/Dashboard';
-import { Footer } from './Components/Footer';
-import { Nav } from './Components/Nav';
+import { Dashboard } from './components/Dashboard/Main.js';
+import { Footer } from './components/Footer';
+import { Nav } from './components/Nav.js';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box>
         {/* <ColorModeSwitcher justifySelf="flex-end" /> */}
-        <Tabs variant='unstyled' colorScheme='teal'>
+        <Tabs variant="unstyled" colorScheme="teal">
           <Nav />
           <TabPanels>
             <TabPanel>
-             <Dashboard />
+              <Dashboard />
             </TabPanel>
             <TabPanel>
               <p>my markets</p>
