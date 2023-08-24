120c496683be570a6e635824809129e73d435c8c
Rae Gaines <rae@throneless.tech>
2023-08-24 12:02:08 -0400

    market page tabs design work

---
+++ client/src/app/icons/star.js
@@ -0,0 +1,16 @@
+import { createIcon } from '@chakra-ui/icons'
+
+const StarIcon = createIcon({
+  displayName: 'StarIcon',
+  viewBox: '0 0 41 40',
+  path: [
+    (
+      <rect width="40" height="40" fill="#E6B223" rx="20" />
+    ),
+    (
+      <path fill="#E6B223" stroke="#F6F5F4" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="m24.8 25.55-3.63 9.97a1.25 1.25 0 0 1-2.34 0l-3.63-9.97a1.27 1.27 0 0 0-.75-.75l-9.97-3.63a1.25 1.25 0 0 1 0-2.34l9.97-3.63a1.27 1.27 0 0 0 .75-.75l3.63-9.97a1.25 1.25 0 0 1 2.34 0l3.63 9.97a1.27 1.27 0 0 0 .75.75l9.97 3.63a1.25 1.25 0 0 1 0 2.34l-9.97 3.63a1.26 1.26 0 0 0-.75.75v0Z" />
+    )
+  ]
+})
+
+export default StarIcon;
--- client/src/app/markets/page.js
+++ client/src/app/markets/page.js
@@ -4,7 +4,9 @@ import {
     ChakraProvider,
     Box,
     Button,
+    Container,
     Flex,
+    Divider,
     Heading,
     HStack,
     Spacer,
@@ -17,11 +19,17 @@ import {
     Tag,
     Text,
 } from '@chakra-ui/react';
+
+// components
 import Dashboard from '../Components/Home.js';
 import Markets from '../Components/Markets/Main.js';
 import Nav from '../Components/Nav.js';
 import SalesPanel from '../Components/SalesPanel/Main.js';
+
 import theme from '../theme.js';
+
+// icons
+import StarIcon from '../icons/star'
 
 function App() {
     return (
@@ -45,49 +53,91 @@ function App() {
                             />
                             <TabPanels>
                                 <TabPanel>
-                                    <Flex spacing={8}>
-                                        <Heading as="h1" color={"gray.700"} textTransform={"uppercase"}>
-                                            Market name
-                                        </Heading>
-                                        <Spacer />
-                                        <HStack spacing={4}>
-                                            <Button size="sm" borderColor={"gray.700"}>
-                                                Review market applications
-                                            </Button>
-                                            <Button size="sm" borderColor={"gray.700"}>
-                                                Create a new season
-                                            </Button>
-                                        </HStack>
-                                    </Flex>
-                                    <Box
-                                        direction="row"
-                                        justify="flex-start"
-                                        align="stretch"
-                                        spacing="24px"
-                                        borderColor="#60A29B"
-                                        borderStartWidth="2px"
-                                        borderEndWidth="2px"
-                                        borderBottomRadius="8px"
-                                        borderBottomWidth="2px"
-                                        borderTop="2px solid #6D635B"
-                                    >
-                                        <Box background="green.600" padding={6}>
-                                            <Flex borderBottom={"2px solid #F6F5F4"} paddingBottom={6}>
+                                    <Container maxW={"6xl"}>
+                                        <Flex spacing={8}>
+                                            <Heading as="h1" color={"gray.700"} textTransform={"uppercase"}>
+                                                Market name
+                                            </Heading>
+                                            <Spacer />
+                                            <HStack spacing={4}>
+                                                <Button size="sm" borderColor={"gray.700"}>
+                                                    Review market applications
+                                                </Button>
+                                                <Button size="sm" borderColor={"gray.700"}>
+                                                    Create a new season
+                                                </Button>
+                                            </HStack>
+                                        </Flex>
+                                        <Box
+                                            direction="row"
+                                            justify="flex-start"
+                                            align="stretch"
+                                            spacing="24px"
+                                            borderBottomRadius="8px"
+                                            borderTop="2px solid #6D635B"
+                                            marginTop={8}
+                                        >
+                                            <Box background="green.600" padding={6}>
+                                                <Flex borderBottom={"2px solid #F6F5F4"} paddingBottom={6}>
+                                                    <HStack>
+                                                        <Text as={"span"} color={"gray.50"} fontFamily={"Zilla Slab"} fontSize="2xl" fontWeight={700} textTransform={"uppercase"}>Market name</Text>
+                                                        <Text as={"span"} color={"gray.50"} fontSize="2xl" textTransform={"uppercase"}>Dates</Text>
+                                                    </HStack>
+                                                    <Spacer />
+                                                    <HStack>
+                                                        <Text color={"gray.50"} fontSize="xs" fontWeight={700} textAlign={"right"} textTransform={"uppercase"} width={24}>
+                                                            Accepting applications
+                                                        </Text>
+                                                        <StarIcon height={8} width={8} />
+                                                    </HStack>
+                                                </Flex>
+                                                <Flex marginTop={4}>
+                                                    <HStack>
+                                                        <Text as={"span"} color={"gray.50"} fontSize="xl" fontWeight={700}>
+                                                            Day, start-endtime
+                                                        </Text>
+                                                        <Text as={"span"} color={"gray.50"} fontSize="xl">
+                                                            Street address, City, State, Zip
+                                                        </Text>
+                                                    </HStack>
+                                                    <Spacer />
+                                                    <HStack>
+                                                        <Text as={"span"} color={"gray.50"} fontSize="xl" fontWeight={700}>
+                                                            Manager:
+                                                        </Text>
+                                                        <Text as={"span"} color={"gray.50"} fontSize="xl">
+                                                            Manager name
+                                                        </Text>
+                                                        <Text as={"span"} color={"gray.50"} fontSize="xl">
+                                                            202-123-4567
+                                                        </Text>
+                                                    </HStack>
+                                                </Flex>
+                                                <Text marginTop={4}>
+                                                    In a vibrant community gathering space, in one of DC's most diverse neighborhoods, a powerhouse boasting a bountiful roster of vendors. With products ranging from fresh produce, to authentic Mexican food prepared with locally grown ingredients, this market's offerings are as dynamic as the neighborhood itself.
+                                                </Text>
+                                            </Box>
+                                            <Box background={"#90B132"} borderBottomRadius="8px" padding={4}>
                                                 <HStack>
-                                                    <Text as={"span"} color={"gray.50"} textTransform={"uppercase"}>Market name</Text>
-                                                    <Text as={"span"} color={"gray.50"}>Dates</Text>
-                                                </HStack>
-                                                <Spacer />
-                                                <HStack>
-                                                    <Text>
-                                                        Accepting applications
+                                                    <Text fontSize={"xs"} textTransform={"uppercase"} fontWeight={700}>
+                                                        Market needs:
                                                     </Text>
+                                                    <Tag bg={"gray.50"} fontWeight={700}>Meat</Tag>
                                                 </HStack>
-                                            </Flex>
-
+                                            </Box>
                                         </Box>
-
-                                    </Box>
+                                    </Container>
+                                    <Container marginTop={4} maxW={"5xl"}>
+                                        <HStack>
+                                            <Text color={"gray.700"} fontWeight={700} textTransform={"uppercase"} width={40}>
+                                                Market size
+                                            </Text>
+                                            <Text color={"gray.600"} fontFamily={"Zilla Slab"} fontWeight={700} textTransform={"uppercase"}>
+                                                Flagship
+                                            </Text>
+                                            <Divider sx={{ borderColor: "gray.600", borderBottomWidth: 2 }} />
+                                        </HStack>
+                                    </Container>
                                 </TabPanel>
                                 <TabPanel>
                                     <p>Reports coming soon.</p>
