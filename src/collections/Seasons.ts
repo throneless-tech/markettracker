be7f2a089f2ed63a7ce96e4d4e1168a3f211a0a3
Josh King <josh@throneless.tech>
2023-09-18 21:39:27 -0400

    Update fields for Seasons collection.

--- src/collections/Seasons.ts
+++ src/collections/Seasons.ts
@@ -39,6 +39,208 @@ export const Seasons: CollectionConfig = {
       },
     },
     {
+      name: "marketTime",
+      label: "Market Time",
+      type: "group",
+      fields: [
+        {
+          name: "startTime",
+          label: "Start time",
+          type: "date",
+          admin: {
+            date: {
+              pickerAppearance: "timeOnly",
+            },
+            description: "Start Time",
+          },
+        },
+        {
+          name: "endTime",
+          label: "End time",
+          type: "date",
+          admin: {
+            date: {
+              pickerAppearance: "timeOnly",
+            },
+            description: "End Time",
+          },
+        },
+      ],
+    },
+    {
+      name: "vendorSalesType",
+      label: "Vendor Sales Report",
+      type: "select",
+      options: [
+        {
+          label: "Cash & Credit",
+          value: "cash_credit",
+        },
+        {
+          label: "Produce Plus",
+          value: "produce_plus",
+        },
+        {
+          label: "SFMNP",
+          value: "sfmnp",
+        },
+        {
+          label: "WIC",
+          value: "wic",
+        },
+        {
+          label: "WIC/Senior",
+          value: "wic_senior",
+        },
+        {
+          label: "SNAP",
+          value: "snap",
+        },
+        {
+          label: "SNAP Bonus",
+          value: "snap_bonus",
+        },
+        {
+          label: "Credit/Debit Coupon",
+          value: "credit_debit_coupon",
+        },
+        {
+          label: "G World Coupon",
+          value: "g_world_coupon",
+        },
+        {
+          label: "Market Goods Coupon",
+          value: "market_goods_coupon",
+        },
+      ],
+      admin: {
+        description: "Check all that apply",
+      },
+    },
+    {
+      name: "marketReportSalesType",
+      label: "Market Report",
+      type: "select",
+      options: [
+        {
+          label: "Cash & Credit",
+          value: "cash_credit",
+        },
+        {
+          label: "Produce Plus",
+          value: "produce_plus",
+        },
+        {
+          label: "SFMNP",
+          value: "sfmnp",
+        },
+        {
+          label: "WIC",
+          value: "wic",
+        },
+        {
+          label: "WIC/Senior",
+          value: "wic_senior",
+        },
+        {
+          label: "SNAP",
+          value: "snap",
+        },
+        {
+          label: "SNAP Bonus",
+          value: "snap_bonus",
+        },
+        {
+          label: "Credit/Debit Coupon",
+          value: "credit_debit_coupon",
+        },
+        {
+          label: "G World Coupon",
+          value: "g_world_coupon",
+        },
+        {
+          label: "Market Goods Coupon",
+          value: "market_goods_coupon",
+        },
+      ],
+      admin: {
+        description: "Check all that apply",
+      },
+    },
+    {
+      name: "farmersRegisterSalesType",
+      label: "Farmers Register App",
+      type: "select",
+      options: [
+        {
+          label: "Cash & Credit",
+          value: "cash_credit",
+        },
+        {
+          label: "Produce Plus",
+          value: "produce_plus",
+        },
+        {
+          label: "SFMNP",
+          value: "sfmnp",
+        },
+        {
+          label: "WIC",
+          value: "wic",
+        },
+        {
+          label: "WIC/Senior",
+          value: "wic_senior",
+        },
+        {
+          label: "SNAP",
+          value: "snap",
+        },
+        {
+          label: "SNAP Bonus",
+          value: "snap_bonus",
+        },
+        {
+          label: "Credit/Debit Coupon",
+          value: "credit_debit_coupon",
+        },
+        {
+          label: "G World Coupon",
+          value: "g_world_coupon",
+        },
+        {
+          label: "Market Goods Coupon",
+          value: "market_goods_coupon",
+        },
+      ],
+      admin: {
+        description: "Check all that apply",
+      },
+    },
+    {
+      name: "operators",
+      label: "Market Operators",
+      type: "relationship",
+      relationTo: "users",
+      hasMany: true,
+      admin: {
+        description:
+          "Select which Managers will be onsite and supervising during the market.",
+      },
+    },
+    {
+      name: "reviewers",
+      label: "Market Application Reviewers",
+      type: "relationship",
+      relationTo: "users",
+      hasMany: true,
+      minRows: 2,
+      admin: {
+        description:
+          "Select at least 2 Managers who will review and accept applications.",
+      },
+    },
+    {
       name: "productGaps",
       label: "Product Gaps",
       type: "relationship", // TODO placeholder for custom field
