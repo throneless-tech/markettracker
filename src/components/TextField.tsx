c1a7e053c9ded072517262c78564270375971429
Josh King <josh@throneless.tech>
2023-09-19 03:36:47 -0400

    Create custom text field.

---
+++ src/components/TextField.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from "react";
+import { useField } from "payload/components/forms";
+import {
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+
+type Props = {
+  label: string;
+  path: string;
+  required: boolean;
+  admin: {
+    description: string;
+    placeholder: string;
+  };
+};
+
+export const TextField: FC<Props> = (
+  { label, path, required, admin: { description, placeholder } },
+) => {
+  const { value, setValue } = useField<Props>({ path });
+
+  return (
+    <FormControl>
+      <FormLabel>{label + (required ? " (Required)" : "")}</FormLabel>
+      {description ? <FormHelperText>{description}</FormHelperText> : ""}
+      <Input
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </FormControl>
+  );
+};
