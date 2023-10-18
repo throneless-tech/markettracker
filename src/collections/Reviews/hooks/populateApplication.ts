2a60b57975922912675d17d4fea90c59809d392c
Josh King <josh@throneless.tech>
2023-10-17 22:51:48 -0400

    Add missing hook.

---
+++ src/collections/Reviews/hooks/populateApplication.ts
@@ -0,0 +1,29 @@
+import payload from "payload";
+import {
+  CollectionAfterReadHook,
+  CollectionBeforeValidateHook,
+} from "payload/types";
+
+export const afterReadApplication: CollectionAfterReadHook = async ({
+  doc, // full document data
+}) => {
+  if (doc.application && typeof doc.application === "string") {
+    const application = await payload.findByID({
+      id: doc.application,
+      collection: "applications",
+      depth: 1,
+    });
+    return { ...doc, application: application };
+  }
+  return doc;
+};
+
+export const beforeValidateApplication: CollectionBeforeValidateHook = async ({
+  data,
+}) => {
+  if (
+    data.application && typeof data.application === "object"
+  ) {
+    return { ...data, application: data.application.id };
+  }
+};
