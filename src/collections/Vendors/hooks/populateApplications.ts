b4360ba4076d0ae0b80b4fc87763f2fd100c1150
Josh King <josh@throneless.tech>
2023-10-26 13:59:19 -0400

    Add vendor populateApplications hook.

---
+++ src/collections/Vendors/hooks/populateApplications.ts
@@ -0,0 +1,46 @@
+import payload from "payload";
+import {
+  CollectionAfterReadHook,
+  CollectionBeforeValidateHook,
+} from "payload/types";
+import type { Application } from "payload/generated-types";
+
+export const afterReadApplications: CollectionAfterReadHook = async ({
+  doc, // full document data
+}) => {
+  if (
+    doc.applications &&
+    doc.applications.length &&
+    typeof doc.applications[0] === "string"
+  ) {
+    const applications = await payload.find({
+      collection: "applications",
+      depth: 0,
+      where: { id: { in: doc.applications.join(",") } },
+    });
+    return { ...doc, applications: applications.docs };
+  } else if (!doc.applications) {
+    const applications = await payload.find({
+      collection: "applications",
+      depth: 0,
+      where: { vendor: { equals: doc.id } },
+    });
+    return { ...doc, applications: applications.docs };
+  }
+  return doc;
+};
+
+export const beforeValidateApplications: CollectionBeforeValidateHook = async ({
+  data,
+}) => {
+  if (
+    data.applications &&
+    data.applications.length &&
+    typeof data.applications[0] === "object"
+  ) {
+    return {
+      ...data,
+      applications: data.applications.map((app: Application) => app.id),
+    };
+  }
+};
