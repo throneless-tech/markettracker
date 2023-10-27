07aad1314561a2b2dc57407bc9e5655b2895e35b
Josh King <josh@throneless.tech>
2023-10-27 08:21:19 -0400

    Add missing reviews hook.

---
+++ src/collections/Reviews/hooks/addToApplication.ts
@@ -0,0 +1,71 @@
+import payload from "payload";
+import {
+  CollectionAfterChangeHook,
+  CollectionAfterDeleteHook,
+} from "payload/types";
+import type { Review } from "payload/generated-types";
+
+export const afterChangeAddToApplication: CollectionAfterChangeHook = async ({
+  doc,
+  operation,
+}) => {
+  console.log("***doc", doc);
+  if (operation === "create") {
+    const application = await payload.findByID({
+      id: doc.application.id,
+      collection: "applications",
+      depth: 0,
+    });
+    console.log("***application:", application);
+
+    let reviews: string[] = [];
+    if (application.reviews && application.reviews.length) {
+      if (typeof application.reviews[0] === "object") {
+        reviews = application.reviews.map((review: any) => review.id);
+      } else {
+        reviews = application.reviews as string[];
+      }
+    }
+    await payload.update({
+      id: doc.application.id,
+      collection: "applications",
+      depth: 0,
+      data: {
+        reviews: [...reviews, doc.id],
+      },
+    });
+  }
+};
+
+export const afterDeleteRemoveFromApplication: CollectionAfterDeleteHook =
+  async ({ doc }) => {
+    const application = await payload.findByID({
+      id: doc.application.id,
+      collection: "applications",
+      depth: 0,
+    });
+
+    if (application.reviews && application.reviews.length) {
+      let reviews: string[];
+      if (typeof application.reviews[0] === "object") {
+        reviews = application.reviews.reduce((acc, review: Review) => {
+          if (review.id !== doc.id) {
+            acc.push(review.id);
+          }
+          return acc;
+        }, []);
+      } else {
+        reviews = application.reviews.filter(
+          (review) => review !== doc.id,
+        ) as string[];
+      }
+      await payload.update({
+        id: doc.application.id,
+        collection: "applications",
+        depth: 0,
+        data: {
+          reviews,
+        },
+      });
+    }
+  };
