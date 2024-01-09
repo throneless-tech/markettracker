4743097143acb52f5c9cf950b433af7cf8431c70
Harum <harum@throneless.tech>
2024-01-09 11:33:07 -0500

    Add top margins

--- src/components/SalesReports/CustomSalesReportsEdit.tsx
+++ src/components/SalesReports/CustomSalesReportsEdit.tsx
@@ -48,67 +48,69 @@ const CustomSalesReportsEdit: React.FC<any> = () => {
           ) : null}
         </Flex>
         <Divider color="gray.900" borderBottomWidth={2} opacity={1} />
-        <FormControl isRequired>
-          <Grid templateColumns="repeat(2, 5fr)" gap={4}>
-            <GridItem>
-              <FormLabel>Cash and credit sales (required)</FormLabel>
-              <Input
-                //   value=
-                //   onChange={}
-                isRequired
-                placeholder="Cash and credit sales"
-              />
-              <FormHelperText>
-                Enter the sum total of Cash and Credit sales
-              </FormHelperText>
-            </GridItem>
-            <GridItem w="100%" h="10">
-              <FormLabel>Produce Plus sales (required)</FormLabel>
-              <Input
-                //   value={}
-                //   onChange={}
-                isRequired
-                placeholder="Produce plus sales"
-                //   type="email"
-              />
-              <FormHelperText>
-                Enter the sum total of Produce Plus sales
-              </FormHelperText>
-            </GridItem>
-          </Grid>
-          <Grid templateColumns="repeat(2, 5fr)" gap={4}>
-            <GridItem>
-              <FormLabel>SFMNP sales (required)</FormLabel>
-              <Input
-                //   value=
-                //   onChange={}
-                isRequired
-                placeholder="SFMNP sales"
-              />
-              <FormHelperText>
-                Enter the sum total of Seniors Farmers' Market Nutrition Program
-                sales
-              </FormHelperText>
-            </GridItem>
-            <GridItem>
-              <FormLabel>WIC sales (required)</FormLabel>
-              <Input
-                //   value={}
-                //   onChange={}
-                placeholder="WIC sales"
-                //   type="email"
-                isRequired
-              />
-              <FormHelperText>
-                Enter the sum total of Special Supplemental Nutrition Program
-                for Women, Infants, and Children (WIC) sales
-              </FormHelperText>
-            </GridItem>
-          </Grid>
-          <Center marginTop={6}>
-            <Button>Confirm and Submit Sales Report</Button>
-          </Center>
-        </FormControl>
+        <Container maxW="container.xl" marginTop={6} marginBottom={4}>
+          <FormControl isRequired>
+            <Grid templateColumns="repeat(2, 5fr)" gap={4}>
+              <GridItem>
+                <FormLabel>Cash and credit sales (required)</FormLabel>
+                <Input
+                  //   value=
+                  //   onChange={}
+                  isRequired
+                  placeholder="Cash and credit sales"
+                />
+                <FormHelperText>
+                  Enter the sum total of Cash and Credit sales
+                </FormHelperText>
+              </GridItem>
+              <GridItem w="100%" h="10">
+                <FormLabel>Produce Plus sales (required)</FormLabel>
+                <Input
+                  //   value={}
+                  //   onChange={}
+                  isRequired
+                  placeholder="Produce plus sales"
+                  //   type="email"
+                />
+                <FormHelperText>
+                  Enter the sum total of Produce Plus sales
+                </FormHelperText>
+              </GridItem>
+            </Grid>
+            <Grid templateColumns="repeat(2, 5fr)" gap={4}>
+              <GridItem>
+                <FormLabel>SFMNP sales (required)</FormLabel>
+                <Input
+                  //   value=
+                  //   onChange={}
+                  isRequired
+                  placeholder="SFMNP sales"
+                />
+                <FormHelperText>
+                  Enter the sum total of Seniors Farmers' Market Nutrition
+                  Program sales
+                </FormHelperText>
+              </GridItem>
+              <GridItem>
+                <FormLabel>WIC sales (required)</FormLabel>
+                <Input
+                  //   value={}
+                  //   onChange={}
+                  placeholder="WIC sales"
+                  //   type="email"
+                  isRequired
+                />
+                <FormHelperText>
+                  Enter the sum total of Special Supplemental Nutrition Program
+                  for Women, Infants, and Children (WIC) sales
+                </FormHelperText>
+              </GridItem>
+            </Grid>
+            <Center marginTop={6}>
+              <Button>Confirm and Submit Sales Report</Button>
+            </Center>
+          </FormControl>
+        </Container>
       </Container>
       <FooterAdmin />
     </>
