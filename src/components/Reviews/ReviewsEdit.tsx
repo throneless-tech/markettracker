f5f5e56d90a9407beae2b4f291756a1cd03320bf
rudietuesdays <rae@throneless.tech>
2024-02-07 16:31:51 -0500

    update math on reviews aggregate score

--- src/components/Reviews/ReviewsEdit.tsx
+++ src/components/Reviews/ReviewsEdit.tsx
@@ -107,6 +107,7 @@ export const ReviewsEdit: React.FC<any> = () => {
         review.vendorScore;
 
       total += thisScore;
+      total = total / reviews.length;
     });
     return total;
   };
