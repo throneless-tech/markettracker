8dae0c7adf936d79900e5db97d517e38e41793f1
rudietuesdays <rae@throneless.tech>
2024-03-08 14:02:35 -0500

    wip license page

--- src/components/Licenses/LicensesEdit.tsx
+++ src/components/Licenses/LicensesEdit.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "payload/components/utilities";
 import { useField, useForm } from "payload/components/forms";
+import { useHistory } from "react-router-dom";
 
 // Chakra imports
 import {
@@ -22,6 +23,7 @@ import { Vendor } from "payload/generated-types";
 
 export const LicensesEdit: React.FC<any> = () => {
   const { user } = useAuth();
+  const history: any = useHistory();
   const { submit } = useForm();
   const [doSubmit, setDoSubmit] = useState(false);
 
@@ -51,6 +53,7 @@ export const LicensesEdit: React.FC<any> = () => {
       console.log("licenseType", licenseType);
 
       submit();
+      history.push("/admin/collections/licenses");
     }
   }, [doSubmit]);
 
