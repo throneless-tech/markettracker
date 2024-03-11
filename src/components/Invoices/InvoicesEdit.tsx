7183e89295a1c8f6d0131da5a628b4b78fae18b2
Josh King <josh@throneless.tech>
2024-03-11 16:14:35 -0400

    Fixes for InvoicesEdit

--- src/components/Invoices/InvoicesEdit.tsx
+++ src/components/Invoices/InvoicesEdit.tsx
@@ -1,137 +1,114 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import qs from "qs";
+import React, { useCallback, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 // utils and payload
-import { useAuth } from "payload/components/utilities";
 // import getSeasons from "../../utils/getSeasons";
-import { Market, Season, Vendor } from "payload/generated-types";
+import { Invoice } from "payload/generated-types";
 
 // components
-// import { NumberField } from "../fields/NumberField";
-import { Dropdown } from "../Dropdown";
-import { FooterAdmin } from "../FooterAdmin";
-import { useField, useForm } from "payload/components/forms";
 import {
   Box,
   Container,
   Flex,
-  FormControl,
-  FormLabel,
-  FormHelperText,
   Heading,
   Spacer,
-  Stack,
   Table,
   Thead,
   Tbody,
-  Tfoot,
   Tr,
   Th,
   Td,
-  TableCaption,
   TableContainer,
   Text,
-  Select,
-  VStack,
 } from "@chakra-ui/react";
 
 const InvoicesEdit: React.FC<any> = () => {
-  const { user } = useAuth();
   // TODO USER PERMISSIONS
 
-  const [vendors, setVendors] = useState<Vendor[]>([]);
-  const [vendor, setVendor] = useState(null);
   const [salesReports, setSalesReports] = useState([]);
-  const [invoice, setInvoice] = useState([]);
-
-  const handleVendorChange = (event) => {
-    const thisVendor = vendors.find(
-      (vendor) => vendor.id == event.target.value,
-    );
-    setVendor(thisVendor);
-    getSalesReports(event.target.value);
-  };
-
-  const getVendors = async () => {
-    const response = await fetch(`/api/vendors?depth=0&limit=9999`);
-    const json = await response.json();
-    const vendors = json ? json.docs : [];
-    setVendors(vendors);
-  };
-
-  const getSalesReports = async (id) => {
-    const query = {
-      vendor: {
-        equals: id,
-      },
-    };
-    const stringifiedQuery = qs.stringify(
-      {
-        where: query, // ensure that `qs` adds the `where` property, too!
-      },
-      { addQueryPrefix: true },
-    );
-
-    const response = await fetch(`/api/sales-reports${stringifiedQuery}`);
-    let reports = await response.json();
-    reports = reports.docs;
-    setSalesReports(reports);
-  };
+  const [invoice, setInvoice] = useState<Invoice>();
+  const [subtotal, setSubtotal] = useState(0);
+  const history = useHistory();
 
   useEffect(() => {
-    getVendors();
-  }, []);
-
-  function reportReducer(accumulator, report, index) {
-    const existingReport = accumulator.get(report.season.id);
-
-    if (existingReport) {
-      existingReport.cashAndCredit += report.cashAndCredit;
-      existingReport.marketDays += 1;
-      existingReport.producePlus += report.producePlus;
-      existingReport.sfmnp += report.sfmnp;
-      existingReport.wic += report.wic;
-    } else {
-      let thisReport = {
-        ...report,
-        marketDays: 1,
-      };
-      accumulator.set(report.season.id, thisReport);
+    if (history.location.state) {
+      setInvoice(history.location.state as Invoice);
     }
+  }, [history.location.state]);
 
-    return accumulator;
-  }
+  const reportReducer = useCallback(
+    (accumulator, report, index) => {
+      const existingReport = accumulator.get(report.season.id);
+
+      const {
+        producePlus,
+        cashAndCredit,
+        wic,
+        sfmnp,
+        ebt,
+        snapBonus,
+        fmnpBonus,
+        cardCoupon,
+        marketGoods,
+        gWorld,
+      } = report;
+      if (existingReport) {
+        existingReport.cashAndCredit += cashAndCredit;
+        existingReport.marketDays += 1;
+        existingReport.producePlus += producePlus;
+        existingReport.wic += wic;
+        existingReport.sfmnp += sfmnp;
+        existingReport.ebt += ebt;
+        existingReport.snapBonus += snapBonus;
+        existingReport.fmnpBonus += fmnpBonus;
+        existingReport.cardCoupon += cardCoupon;
+        existingReport.marketGoods += marketGoods;
+        existingReport.gWorld += gWorld;
+        existingReport.total +=
+          cashAndCredit -
+          (producePlus ? producePlus : 0) +
+          (wic ? wic : 0) +
+          (sfmnp ? sfmnp : 0) +
+          (ebt ? ebt : 0) +
+          (snapBonus ? snapBonus : 0) +
+          (fmnpBonus ? fmnpBonus : 0) +
+          (cardCoupon ? cardCoupon : 0) +
+          (marketGoods ? marketGoods : 0) +
+          (gWorld ? gWorld : 0);
+        setSubtotal(subtotal + existingReport.total);
+      } else {
+        let thisReport = {
+          ...report,
+          marketDays: 1,
+          total:
+            cashAndCredit -
+            (producePlus ? producePlus : 0) +
+            (wic ? wic : 0) +
+            (sfmnp ? sfmnp : 0) +
+            (ebt ? ebt : 0) +
+            (snapBonus ? snapBonus : 0) +
+            (fmnpBonus ? fmnpBonus : 0) +
+            (cardCoupon ? cardCoupon : 0) +
+            (marketGoods ? marketGoods : 0) +
+            (gWorld ? gWorld : 0),
+        };
+        setSubtotal(subtotal + thisReport.total);
+        accumulator.set(report.season.id, thisReport);
+      }
+
+      return accumulator;
+    },
+    [subtotal],
+  );
 
   useEffect(() => {
-    if (salesReports.length) {
-      const combinedReports = salesReports.reduce(reportReducer, new Map());
+    if (invoice?.reports?.length) {
+      const combinedReports = invoice.reports.reduce(reportReducer, new Map());
       const reportsArray = Array.from(combinedReports.values());
-      setInvoice(reportsArray);
+      setSalesReports(reportsArray);
     }
-  }, [salesReports, vendor, vendors]);
-
-  const calculateNet = (row) => {
-    if (!invoice) {
-      return null;
-    }
-    const rowTotal = row.cashAndCredit - row.producePlus - row.sfmnp - row.wic;
-    return rowTotal;
-  };
-
-  const calculateSubtotal = () => {
-    let subtotal = [];
-
-    invoice.map((row) => {
-      const rowTotal =
-        row.cashAndCredit - row.producePlus - row.sfmnp - row.wic;
-      subtotal.push(rowTotal);
-    });
-
-    const sum = subtotal.reduce((partialSum, a) => partialSum + a, 0);
-
-    return sum;
-  };
+  }, [invoice]);
 
   return (
     <Container maxW="container.2xl" marginBottom={4}>
@@ -142,73 +119,6 @@ const InvoicesEdit: React.FC<any> = () => {
           </Heading>
         </Box>
         <Spacer />
-        <VStack>
-          <FormControl sx={{ alignItems: "center", display: "flex" }}>
-            <FormLabel>
-              <Text
-                fontFamily="Zilla Slab"
-                lineHeight="1"
-                fontWeight="semibold"
-                fontSize={24}
-                letterSpacing="0.03em"
-                textTransform="capitalize"
-                color="gray.600"
-                width={200}
-              >
-                Choose a vendor
-              </Text>
-            </FormLabel>
-            <Select
-              placeholder="Select an option"
-              maxWidth={"360px"}
-              onChange={handleVendorChange}
-              sx={{ color: "gray.700" }}
-            >
-              {vendors.length
-                ? vendors.map((vendor) => {
-                    return (
-                      <option value={vendor.id} key={vendor.id}>
-                        {vendor.name}
-                      </option>
-                    );
-                  })
-                : null}
-            </Select>
-          </FormControl>
-          <FormControl sx={{ alignItems: "center", display: "flex" }}>
-            <FormLabel>
-              <Text
-                fontFamily="Zilla Slab"
-                lineHeight="1"
-                fontWeight="semibold"
-                fontSize="24px"
-                letterSpacing="0.03em"
-                textTransform="capitalize"
-                color="gray.600"
-                width={200}
-              >
-                Choose a date
-              </Text>
-            </FormLabel>
-            <Select
-              placeholder="Select an option"
-              // value={reportDate}
-              maxWidth={"360px"}
-              // onChange={handleDateChange}
-              sx={{ color: "gray.700" }}
-            >
-              {/* {dateOptions && dateOptions.length
-            ? dateOptions.map((dateObj) => {
-                return (
-                    <option value={dateObj.date} key={dateObj.id}>
-                    {new Date(dateObj.date).toLocaleDateString("en-US")}
-                    </option>
-                );
-                })
-            : null} */}
-            </Select>
-          </FormControl>
-        </VStack>
       </Flex>
       <Heading
         as="h2"
@@ -219,7 +129,9 @@ const InvoicesEdit: React.FC<any> = () => {
           textTransform: "uppercase",
         }}
       >
-        {vendor ? vendor.name : ""}
+        {invoice?.vendor && typeof invoice.vendor === "object"
+          ? invoice.vendor.name
+          : ""}
       </Heading>
       <Container maxW="container.xl" marginBottom={4}>
         <Heading as="h3" sx={{ fontSize: 24 }}>
@@ -404,8 +316,8 @@ const InvoicesEdit: React.FC<any> = () => {
               </Tr>
             </Thead>
             <Tbody>
-              {invoice && invoice.length
-                ? invoice.map((row) => (
+              {salesReports && salesReports.length
+                ? salesReports.map((row) => (
                     <Tr>
                       <Td
                         sx={{
@@ -435,7 +347,7 @@ const InvoicesEdit: React.FC<any> = () => {
                           width: 100,
                         }}
                       >
-                        FIXME
+                        {row.cashAndCredit}
                       </Td>
                       <Td
                         isNumeric
@@ -445,7 +357,7 @@ const InvoicesEdit: React.FC<any> = () => {
                           width: 100,
                         }}
                       >
-                        FIXME
+                        {}
                       </Td>
                       <Td
                         isNumeric
@@ -455,7 +367,7 @@ const InvoicesEdit: React.FC<any> = () => {
                           width: 100,
                         }}
                       >
-                        FIXME
+                        {row.ebt}
                       </Td>
                       <Td
                         isNumeric
@@ -465,7 +377,7 @@ const InvoicesEdit: React.FC<any> = () => {
                           width: 100,
                         }}
                       >
-                        FIXME
+                        {row.snapBonus}
                       </Td>
                       <Td
                         isNumeric
@@ -485,7 +397,7 @@ const InvoicesEdit: React.FC<any> = () => {
                           width: 100,
                         }}
                       >
-                        {row.cashAndCredit}
+                        {row.cardCoupon}
                       </Td>
                       <Td
                         isNumeric
@@ -495,7 +407,7 @@ const InvoicesEdit: React.FC<any> = () => {
                           width: 100,
                         }}
                       >
-                        FIXME
+                        {row.marketGoods}
                       </Td>
                       <Td
                         isNumeric
@@ -505,10 +417,10 @@ const InvoicesEdit: React.FC<any> = () => {
                           width: 100,
                         }}
                       >
-                        FIXME
+                        {row.gWorld}
                       </Td>
                       <Td isNumeric sx={{ fontWeight: "bold" }}>
-                        {calculateNet(row)}
+                        {row.total}
                       </Td>
                     </Tr>
                   ))
@@ -519,7 +431,7 @@ const InvoicesEdit: React.FC<any> = () => {
         <Flex align="center" justify="flex-end" marginTop={6}>
           <Text sx={{ backgroundColor: "teal.100", padding: 4 }}>Subtotal</Text>
           <Text sx={{ backgroundColor: "teal.50", padding: 4 }}>
-            {calculateSubtotal()}
+            {subtotal}
           </Text>
         </Flex>
         <Heading as="h3" marginTop={12} sx={{ fontSize: 24 }}>
@@ -602,7 +514,7 @@ const InvoicesEdit: React.FC<any> = () => {
                     maxWidth: 300,
                   }}
                 >
-                  FIXME
+                  Example Market
                 </Td>
                 <Td
                   isNumeric
@@ -612,7 +524,7 @@ const InvoicesEdit: React.FC<any> = () => {
                     width: 100,
                   }}
                 >
-                  FIXME
+                  $0
                 </Td>
                 <Td
                   sx={{
@@ -638,7 +550,7 @@ const InvoicesEdit: React.FC<any> = () => {
         </TableContainer>
         <Flex align="center" justify="flex-end" marginTop={6}>
           <Text sx={{ backgroundColor: "teal.100", padding: 4 }}>Subtotal</Text>
-          <Text sx={{ backgroundColor: "teal.50", padding: 4 }}>$FIXME</Text>
+          <Text sx={{ backgroundColor: "teal.50", padding: 4 }}>0</Text>
         </Flex>
         <Flex align="center" justify="flex-end" marginTop={6}>
           <Text sx={{ backgroundColor: "teal.100", padding: 4 }}>
@@ -652,7 +564,7 @@ const InvoicesEdit: React.FC<any> = () => {
               padding: 4,
             }}
           >
-            $FIXME
+            {subtotal}
           </Text>
         </Flex>
       </Container>
