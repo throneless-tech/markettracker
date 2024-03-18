fec2a31821b827404ba98b39cce79082153a7494
Harum <harum@throneless.tech>
2024-03-18 18:29:34 -0400

    Add column to determine who needs to make this action

--- src/components/SalesReports/CustomSalesReportsList.tsx
+++ src/components/SalesReports/CustomSalesReportsList.tsx
@@ -108,6 +108,58 @@ const CustomSalesReportsList: React.FC<any> = () => {
     });
   };
 
+  const needsAction = (report) => {
+    const {
+      season,
+      invoiceDate,
+      day,
+      producePlus,
+      cashAndCredit,
+      wic,
+      sfmnp,
+      ebt,
+      snapBonus,
+      fmnpBonus,
+      cardCoupon,
+      marketGoods,
+      gWorld,
+      penalty,
+    } = report;
+
+    if (
+      !cashAndCredit &&
+      (!producePlus ||
+        !wic ||
+        !sfmnp ||
+        !ebt ||
+        !snapBonus ||
+        !fmnpBonus ||
+        !cardCoupon ||
+        !marketGoods ||
+        !gWorld)
+    ) {
+      return "Staff & vendor actions needed";
+    }
+
+    if (!cashAndCredit) {
+      return "Vendor action needed";
+    }
+
+    if (
+      !producePlus ||
+      !wic ||
+      !sfmnp ||
+      !ebt ||
+      !snapBonus ||
+      !fmnpBonus ||
+      !cardCoupon ||
+      !marketGoods ||
+      !gWorld
+    ) {
+      return "Staff action needed";
+    }
+  };
+
   const handleDelete = async (report) => {
     // TODO: A POPUP FOR "ARE YOU SURE YOU WANT TO DELETE THIS REPORT"
     try {
@@ -377,6 +429,11 @@ const CustomSalesReportsList: React.FC<any> = () => {
                 <Th
                   sx={{ color: "gray.900", fontFamily: "'Outfit', sans-serif" }}
                 >
+                  Action needed
+                </Th>
+                <Th
+                  sx={{ color: "gray.900", fontFamily: "'Outfit', sans-serif" }}
+                >
                   Penalties/Credits
                 </Th>
                 <Th
@@ -419,6 +476,7 @@ const CustomSalesReportsList: React.FC<any> = () => {
                       cardCoupon,
                       marketGoods,
                       gWorld,
+                      penalty,
                     } = report;
                     return (
                       <Tr>
@@ -429,7 +487,8 @@ const CustomSalesReportsList: React.FC<any> = () => {
                             : null}
                         </Td>
                         <Td>{new Date(day).toLocaleDateString("en-US")}</Td>
-                        <Td>$0</Td>
+                        <Td>{needsAction(report)}</Td>
+                        <Td>{penalty ? `$${penalty}` : "$0"}</Td>
                         <Td>{cashAndCredit ? `$${cashAndCredit}` : ""}</Td>
                         <Td>
                           $
