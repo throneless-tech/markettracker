7a67845c2212db9ce04b42577a65f3d84ceb2f9b
Harum <harum@throneless.tech>
2024-03-24 19:59:31 -0400

    Added logic to hide readyToInvoice from vendors

--- src/components/Invoices/InvoicesTabs.tsx
+++ src/components/Invoices/InvoicesTabs.tsx
@@ -4,7 +4,7 @@ import { Link as ReactRouterLink } from "react-router-dom";
 // Chakra imports
 import { Box, HStack, Link as ChakraLink } from "@chakra-ui/react";
 
-export const InvoicesTabs: React.FC<any> = ({ selected }) => {
+export const InvoicesTabs: React.FC<any> = ({ role, selected }) => {
   return (
     <Box as="nav" aria-label="Component navigation" bg={"gray.50"}>
       <HStack
@@ -35,25 +35,27 @@ export const InvoicesTabs: React.FC<any> = ({ selected }) => {
             </Box>
           </ChakraLink>
         </Box>
-        <Box as="li">
-          <ChakraLink href="/admin/collections/invoices?where[exported][equals]=false">
-            <Box
-              mb="-1px"
-              display="block"
-              px="5"
-              py="3"
-              borderBottom="2px solid transparent"
-              data-selected={selected == "applications" ? "" : undefined}
-              _selected={{
-                borderColor: "teal.500",
-                color: "#000",
-                fontWeight: "700",
-              }}
-            >
-              Ready to invoice
-            </Box>
-          </ChakraLink>
-        </Box>
+        {role !== "vendor" ? (
+          <Box as="li">
+            <ChakraLink href="/admin/collections/invoices?where[exported][equals]=false">
+              <Box
+                mb="-1px"
+                display="block"
+                px="5"
+                py="3"
+                borderBottom="2px solid transparent"
+                data-selected={selected == "readyToInvoice" ? "" : undefined}
+                _selected={{
+                  borderColor: "teal.500",
+                  color: "#000",
+                  fontWeight: "700",
+                }}
+              >
+                Ready to invoice
+              </Box>
+            </ChakraLink>
+          </Box>
+        ) : null}
         <Box as="li">
           <ChakraLink href="/admin/collections/invoices">
             <Box
@@ -62,7 +64,7 @@ export const InvoicesTabs: React.FC<any> = ({ selected }) => {
               px="5"
               py="3"
               borderBottom="2px solid transparent"
-              data-selected={selected == "applications" ? "" : undefined}
+              data-selected={selected == "invoices" ? "" : undefined}
               _selected={{
                 borderColor: "teal.500",
                 color: "#000",
