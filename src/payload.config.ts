9f23c3d177488fcaa5bccac3be32a866746ffdee
Josh King <josh@throneless.tech>
2024-03-27 20:57:00 -0500

    Disable useFacet for use with DocumentDB

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -119,6 +119,9 @@ export default buildConfig({
   ],
   cors: ["*", "https://markettracker.payloadcms.app"],
   db: mongooseAdapter({
+    connectOptions: {
+      useFacet: false,
+    },
     url: process.env.DATABASE_URI,
   }),
   debug: true,
