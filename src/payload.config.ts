143d3c95f221ce63879bdcf1fb66a70aa10378ed
Josh King <josh@throneless.tech>
2024-03-29 16:00:20 -0500

    Disable autoindexing in mongoose settings

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -115,6 +115,7 @@ export default buildConfig({
   cors: ["*", "https://markettracker.payloadcms.app"],
   db: mongooseAdapter({
     connectOptions: {
+      autoIndex: false,
       useFacet: false,
     },
     url: process.env.DATABASE_URI,
