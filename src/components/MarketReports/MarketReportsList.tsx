fba69f0e2944421f2f085c45e0038615d733680c
rudietuesdays <rae@throneless.tech>
2024-04-05 16:10:29 -0400

    wip converting market reports list cards to table

--- src/components/MarketReports/MarketReportsList.tsx
+++ src/components/MarketReports/MarketReportsList.tsx
@@ -11,7 +11,14 @@ import {
   Grid,
   GridItem,
   Heading,
+  Table,
+  TableContainer,
   Text,
+  Tbody,
+  Thead,
+  Td,
+  Th,
+  Tr,
 } from "@chakra-ui/react";
 
 // components
@@ -22,6 +29,28 @@ import { SeasonsTabs } from "../Seasons/SeasonsTabs";
 export const MarketReportsList: React.FC<any> = () => {
   const { user } = useAuth();
   const [markets, setMarkets] = useState([]);
+
+  // figure out which market days are in the future
+  const options: any = {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  };
+
+  const nextMarketDate = (startDate) => {
+    let today = new Date();
+    const date = new Date(startDate);
+    const dayNum = date.getDay();
+
+    // show correct date for market reports if it is before market begins
+    if (today < date) {
+      date.setDate(date.getDate() + ((dayNum + (7 - date.getDay())) % 7));
+      return date.toLocaleDateString("en-US", options);
+    } else {
+      today.setDate(today.getDate() + ((dayNum - (7 + today.getDay())) % 7));
+      return today.toLocaleDateString("en-US", options);
+    }
+  };
 
   // query and call for operator-specific markets
   const query = {
@@ -78,25 +107,98 @@ export const MarketReportsList: React.FC<any> = () => {
               Choose a market
             </Text>
           ) : null}
-          <Grid
-            templateColumns={[
-              "repeat(1, 1fr)",
-              "repeat(2, 1fr)",
-              "repeat(3, 1fr)",
-            ]}
-            gap={6}
-            marginTop={6}
-          >
-            {markets && markets.length ? (
-              markets.map((market) => (
-                <GridItem key={market.id}>
-                  <MarketReportCard market={market} />
-                </GridItem>
-              ))
-            ) : (
-              <Text>Your market reports are up to date.</Text>
-            )}
-          </Grid>
+          <TableContainer marginTop={8}>
+            <Table
+              variant="striped"
+              colorScheme={"green"}
+              sx={{ tableLayout: ["auto", "auto", "fixed"], width: "100%" }}
+            >
+              <Thead background={"gray.100"}>
+                <Tr>
+                  <Th
+                    sx={{
+                      color: "gray.900",
+                      fontFamily: "'Outfit', sans-serif",
+                      maxW: 180,
+                    }}
+                  >
+                    Market
+                  </Th>
+                  <Th
+                    sx={{
+                      color: "gray.900",
+                      fontFamily: "'Outfit', sans-serif",
+                      maxW: 180,
+                    }}
+                  >
+                    Market date
+                  </Th>
+                  <Th
+                    sx={{
+                      color: "gray.900",
+                      fontFamily: "'Outfit', sans-serif",
+                      maxW: 180,
+                    }}
+                  >
+                    Operators
+                  </Th>
+                  <Th
+                    sx={{
+                      color: "gray.900",
+                      fontFamily: "'Outfit', sans-serif",
+                      maxW: 180,
+                    }}
+                  >
+                    Due date
+                  </Th>
+                  <Th />
+                </Tr>
+              </Thead>
+              <Tbody>
+                {markets && markets.length ? (
+                  markets.map((market) => (
+                    <Tr key={market.id}>
+                      <Td
+                        sx={{
+                          inlineSize: 180,
+                          maxW: 180,
+                          whiteSpace: "normal",
+                          wordBreak: "break-all",
+                        }}
+                      >
+                        {market.name}
+                      </Td>
+                      <Td
+                        sx={{
+                          inlineSize: 180,
+                          maxW: 180,
+                          whiteSpace: "normal",
+                          wordBreak: "break-all",
+                        }}
+                      >
+                        {nextMarketDate(market.marketDates.startDate)}
+                      </Td>
+                      <Td>
+                        {market.operators && market.operators.length
+                          ? market.operators.map((operator, index) => {
+                              if (index === market.operators.length - 1) {
+                                return operator.name;
+                              } else {
+                                return `${operator.name}, `;
+                              }
+                            })
+                          : null}
+                      </Td>
+                    </Tr>
+                  ))
+                ) : (
+                  <Tr>
+                    <Td>Your market reports are up to date.</Td>
+                  </Tr>
+                )}
+              </Tbody>
+            </Table>
+          </TableContainer>
         </Box>
       </Container>
       <FooterAdmin />
