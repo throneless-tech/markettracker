4db34bb3f20aa3a029469a3659241dfc09f9f9d8
Rae Gaines <rae@throneless.tech>
2024-11-18 15:21:28 -0500

    Revert "add email from resend to markettracker"
    This reverts commit da846e556784fdf90f06ddebc86a9594e72549de.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import payload from "payload";
 import path from "path";
-import nodemailer from 'nodemailer';
 
 require("dotenv").config();
 const app = express();
@@ -17,22 +16,10 @@ app.get("/", (_, res) => {
   res.redirect("/admin");
 });
 
-// set up email capabilities
-const transport = async () => {
-  const transport = await nodemailer.createTransport({
-    apiKey: process.env.RESEND_API_KEY,
-  })
-};
-
 // Initialize Payload
 payload.init({
   secret: process.env.PAYLOAD_SECRET,
   express: app,
-  email: {
-    fromName: 'FreshFarm MarketTracker',
-    fromAddress: 'no-reply@markettracker.payloadcms.app',
-    transport,
-  },
   loggerOptions: { level: "debug" },
   onInit: () => {
     payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
